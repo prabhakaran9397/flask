2ed9af0472d47ef87cdfa58c08306a58c2cc4325
Prabhakaran Senthilnathan <prabhakaran9397@gmail.com>
2016-05-03 15:02:48 +0530

    working

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
-	return "Home"
+	return render_template('index.html')
 
-@app.route("/help")
-def help():
-	return "Help"
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
 
-if __name__ == "__main__":
+@app.route('/signUp', methods=['POST'])
+def signUp():
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+
+	if _name and _email and _password:
+		return json.dumps({'html':'<span>All fields good !!</span>'})
+	else:
+		return json.dumps({'html':'<span>Enter the required fields</span>'})
+    
+if __name__ == '__main__':
 	app.run()
